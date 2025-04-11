f3fa2600d00a2e9ff5874c6d53eb5a185ac84b07
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-04-11 21:25:22 +0600

    All prerequisites added.

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import cors from 'cors';
+import router from "./app/routers";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+  ],
+  credentials:true
+}));
+
+
+app.use('/api', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({ message: 'Server running....' });
+});
+
+
+app.use(globalErrorHandler)
+
+export default app;
---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from "express";
+import { TUserRole } from "../types/global";
+import catchAsync from "../utils/catchAsync";
+import AppError from "../errors/AppError";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { UserModel } from "../modules/auth/auth.model";
+
+const auth = (...requiredRoles: TUserRole[]) => {
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.headers.authorization;
+        if (!token) {
+            throw new AppError(401, "You are not authorized.");
+        }
+        let decode;
+        try {
+            decode = jwt.verify(token, config.jwt_access_secret as string) as JwtPayload;
+        } catch (err) {
+            throw new AppError(401, "You are not authorized.");
+        }
+        // console.log("Token User", decode);
+        const role = decode.role;
+
+        if (requiredRoles && !requiredRoles.includes(role)) {
+            throw new AppError(401, "You are not authorized.");
+        }
+
+        const user = await UserModel.findOne({ email: decode.email });
+
+        if (!user) {
+            throw new AppError(401, "This user in not found!");
+        }
+        req.user = decode;
+        next();
+    })
+}
+
+export default auth;
