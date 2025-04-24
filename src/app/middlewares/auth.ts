38e9f0a978e22d16dffc8e33450d8f246bf24457
Md. Saiful islam <isaiful.islam508@gmail.com>
2025-04-24 22:55:39 +0600

    removed console

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -6,36 +6,6 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 import User from "../module/user/user.model";
 
-// const auth = (...requiredRoles: TUserRole[]) => {
-//     return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-//         const token = req.headers.authorization;
-//         if (!token) {
-//             throw new AppError(401, "You are not authorized.");
-//         }
-//         let decode;
-//         try {
-//             decode = jwt.verify(token, config.jwt_secret as string) as JwtPayload;
-//         } catch (err) {
-//             throw new AppError(401, "You are not authorized.");
-//         }
-//         // console.log("Token User", decode);
-//         const role = decode.role;
-
-//         if (requiredRoles && !requiredRoles.includes(role as TUserRole)) {
-//             throw new AppError(401, "You are not authorized.");
-//         }
-
-//         const user = await User.findOne({ email: decode.email });
-
-//         if (!user) {
-//             throw new AppError(401, "This user in not found!");
-//         }
-//         req.user = decode;
-//         next();
-//     })
-// }
-
-
 const auth = (...requiredRoles: string[]) => {
     return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
       const token = req.headers.authorization;
@@ -43,7 +13,6 @@ const auth = (...requiredRoles: string[]) => {
       if (!token) {
         throw new AppError(401, "You are not authorized.");
       }
-  
       let decode;
       try {
         decode = jwt.verify(token, config.jwt_secret as string) as JwtPayload;
@@ -56,8 +25,7 @@ const auth = (...requiredRoles: string[]) => {
       if (!requiredRoles.includes(role)) {
         throw new AppError(401, "You are not authorized.It is not your role.");
       }
-  
-      const user = await User.findOne({ _id: decode.id });
+      const user = await User.findOne({ _id: decode._id || decode.id });
       if (!user) {
         throw new AppError(401, "User is not found!");
       }
